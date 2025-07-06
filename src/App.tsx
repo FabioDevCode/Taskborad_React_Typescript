21dc10b3c7526949e323f34be43f1e687c4d2981
FabioDevCode <0677384849@orange.fr>
2025-07-06 15:17:55 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>TaskBoard</h1>
+      <p>Bienvenue sur ton premier projet React + TypeScript 🎯</p>
+    </div>
+  );
+};
+
+export default App;
